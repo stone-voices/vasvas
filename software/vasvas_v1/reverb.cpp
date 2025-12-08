87810b20900545ac342b1ed8480057271cd93915
Vasily <41252163+flutomax@users.noreply.github.com>
2025-12-08 13:12:29 +0700

    Update reverb.cpp

--- software/vasvas_v1/reverb.cpp
+++ software/vasvas_v1/reverb.cpp
@@ -11,36 +11,36 @@ void TReverb::SetSampleRate(float samplerate)
 
   fDelaySamples = fSampleRate / TDelayCell::FsD * (4453 + 3720 + 4217 + 3163);
 
-  delay1_tank.SetParams(fSampleRate, 4453);
-  delay3_tank.SetParams(fSampleRate, 3720);
-  delay2_tank.SetParams(fSampleRate, 4217);
-  delay4_tank.SetParams(fSampleRate, 3163);
+  fTankDelay1.SetParams(fSampleRate, 4453);
+  fTankDelay3.SetParams(fSampleRate, 3720);
+  fTankDelay2.SetParams(fSampleRate, 4217);
+  fTankDelay4.SetParams(fSampleRate, 3163);
 
-  allpass1.SetParams(fSampleRate, 142, 0.998956919f);
-  allpass2.SetParams(fSampleRate, 107, 0.998956919f);
-  allpass3.SetParams(fSampleRate, 379, 0.708165646f);
-  allpass4.SetParams(fSampleRate, 277, 0.708165646f);
+  fAllpass1.SetParams(fSampleRate, 142, 0.998956919f);
+  fAllpass2.SetParams(fSampleRate, 107, 0.998956919f);
+  fAllpass3.SetParams(fSampleRate, 379, 0.708165646f);
+  fAllpass4.SetParams(fSampleRate, 277, 0.708165646f);
 
-  allpass5.SetParams(fSampleRate, 1800);
-  allpass6.SetParams(fSampleRate, 2656);
-  allpass7.SetParams(fSampleRate, 1511);
-  allpass8.SetParams(fSampleRate, 2003);
-  allpass9.SetParams(fSampleRate, 1709);
-  allpass10.SetParams(fSampleRate, 2411);
+  fAllpass5.SetParams(fSampleRate, 1800);
+  fAllpass6.SetParams(fSampleRate, 2656);
+  fAllpass7.SetParams(fSampleRate, 1511);
+  fAllpass8.SetParams(fSampleRate, 2003);
+  fAllpass9.SetParams(fSampleRate, 1709);
+  fAllpass10.SetParams(fSampleRate, 2411);
 
-  mod_allpass1.SetParams(fSampleRate, 672, 0.6f, 24);
-  mod_allpass2.SetParams(fSampleRate, 908, 0.8f, 48);
+  fModAllpass1.SetParams(fSampleRate, 672, 0.6f, 24);
+  fModAllpass2.SetParams(fSampleRate, 908, 0.8f, 48);
 
   fSmootherSize.SetSmoothTime(fSampleRate, 300);
   fSmootherDecay.SetSmoothTime(fSampleRate, 100);
   fSmootherTankDiff.SetSmoothTime(fSampleRate, 100);
 
-  input_lowpass.SetParams(fSampleRate, 10000);
-  input_highpass.SetParams(fSampleRate, 60);
-  tank_lp1.SetParams(fSampleRate, 10000);
-  tank_lp2.SetParams(fSampleRate, 10000);
-  tank_hp1.SetParams(fSampleRate, 20);
-  tank_hp2.SetParams(fSampleRate, 20);
+  fInpLPF.SetParams(fSampleRate, 10000);
+  fInpHPF.SetParams(fSampleRate, 60);
+  fTankLPF1.SetParams(fSampleRate, 10000);
+  fTankLPF2.SetParams(fSampleRate, 10000);
+  fTankHPF1.SetParams(fSampleRate, 20);
+  fTankHPF2.SetParams(fSampleRate, 20);
 
   fA1 = fA2 = fX1 = fX2 = fX3 = fX4 = fX5 = fX6 = fX7 = fX8 = fX9 = fX10 = 0;
   fTankAcc1 = fTankAcc2 = 0;
@@ -89,43 +89,43 @@ void TReverb::Process(float in, float& outL, float& outR)
     fTankDiffSm = fSmootherTankDiff.Process(fTankDiff);   
 
     // Input Filters
-    x = input_highpass.Process(input_lowpass.Process(in));
+    x = fInpHPF.Process(fInpLPF.Process(in));
 
     // Input Diffusers
-    x = allpass1.Process(x);
-    x = allpass2.Process(x);
-    x = allpass3.Process(x);
-    x = allpass4.Process(x);
+    x = fAllpass1.Process(x);
+    x = fAllpass2.Process(x);
+    x = fAllpass3.Process(x);
+    x = fAllpass4.Process(x);
 
     // Tank
-    fA1 = mod_allpass1.Process(x + fTankAcc2, fSizeSm);
-    fA2 = mod_allpass2.Process(x + fTankAcc1, fSizeSm);
+    fA1 = fModAllpass1.Process(x + fTankAcc2, fSizeSm);
+    fA2 = fModAllpass2.Process(x + fTankAcc1, fSizeSm);
 
     // Delay lines (1 and 3)
-    fX1 = delay1_tank.Process(fA1);
-    fX2 = delay3_tank.Process(fA2);
+    fX1 = fTankDelay1.Process(fA1);
+    fX2 = fTankDelay3.Process(fA2);
 
     // Tank Filtering (Damping)
-    tL = tank_hp1.Process(tank_lp1.Process(fX1));
-    tR = tank_hp2.Process(tank_lp2.Process(fX2));
+    tL = fTankHPF1.Process(fTankLPF1.Process(fX1));
+    tR = fTankHPF2.Process(fTankLPF2.Process(fX2));
 
     // Tank AllPass filters
-    fX3 = allpass5.Process(tL, fTankDiffSm, fSizeSm);
-    fX4 = allpass6.Process(tR, fTankDiffSm, fSizeSm);
+    fX3 = fAllpass5.Process(tL, fTankDiffSm, fSizeSm);
+    fX4 = fAllpass6.Process(tR, fTankDiffSm, fSizeSm);
 
     // Add decay control between the allpass filters and the last delay lines
     fX3 *= fDecaySm;
     fX4 *= fDecaySm;
 
     // Dealy lines (2 and 4)
-    fX5 = delay2_tank.Process(fX3);
-    fX6 = delay4_tank.Process(fX4);
+    fX5 = fTankDelay2.Process(fX3);
+    fX6 = fTankDelay4.Process(fX4);
 
     // Added Allpasses 7 - 10
-    fX7 = allpass7.Process(fX5, fTankDiffSm, fSizeSm);
-    fX8 = allpass8.Process(fX6, fTankDiffSm, fSizeSm);
-    fX9 = allpass9.Process(fX7, fTankDiffSm, fSizeSm);
-    fX10 = allpass10.Process(fX8, fTankDiffSm, fSizeSm);
+    fX7 = fAllpass7.Process(fX5, fTankDiffSm, fSizeSm);
+    fX8 = fAllpass8.Process(fX6, fTankDiffSm, fSizeSm);
+    fX9 = fAllpass9.Process(fX7, fTankDiffSm, fSizeSm);
+    fX10 = fAllpass10.Process(fX8, fTankDiffSm, fSizeSm);
 
     // Compute the accumulators as the outputs from the last tank nodes scaled by decay control
     fTankAcc1 = fDecaySm * fX9;
